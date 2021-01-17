fc73d2649df2bc05edee4f664f1b48a75297de75
joey.lybaert <joey.lybaert@student.howest.be>
2021-01-17 06:15:39 +0100

    can now work with all variables

--- README.markdown
+++ README.markdown
@@ -1,14 +1,15 @@
-# Lisp
-
-
-
-
-
-
-
-
-## Usage
-
+<h1> Lisp</h1>
+<p>in deze repo kan u wat meer info vinden over lisp meer bepaald common lisp</p> 
+<h1>opzetten<h1>
+<h2>implementaties</h2>
+<p>vooraleer u kan beginnen moet u eerst een implementatie van common lisp kiezen, voor dit project heb ik steel bank common lisp gebruikt of sbcl afgekort</p>
+<p> ik koos hiervoor voor de goede debugger, het heeft u zeer snel warnings , iets wat ik wel kon gebruiken bij mijn alereerste lisp project</p>
+<h2>ide<h2>
+<p>uw keuze hierin is zeer beperkt en hangt grotendeels van uw os af ook</p>
+<p>hieronder lijst ik de populaire met hun platformen</p>
+<ul>
+<li>allegro common lisp</li>
+</ul>
 
 
 
--- src/main.lisp
+++ src/main.lisp
@@ -1,6 +1,6 @@
 (require "asdf")
 (defpackage lisp
-  (:use :cl :cl-json )
+  (:use :cl :cl-json :for)
   (:export
    :main))
 (in-package :lisp)
@@ -16,30 +16,24 @@
    (coverurl :initarg :coverurl :accessor isebook)
    )
   )
-(defun json-get (json_obj fields)
-  (if (null fields) ; base case of recursion
-      json_obj
-    (let* ((cur-key (car fields))
-           (current (cond ((and (integerp cur-key) 
-                                (eq (car json_obj) 'json-array))
-                           (nth (1+ cur-key) json_obj)) ; add 1 to skip over JSON-ARRAY
-                          ((and (stringp cur-key) 
-                                (eq (car json_obj) 'json-obj))
-                           (second (assoc cur-key (cdr json_obj) :test #'string=))) ; Use CDR to skip over JSON-OBJ
-                          (t (error "~S is not a JSON object or array or ~s is not appropriate key" json_obj cur-key)))))
-      (json-get current (cdr fields)))))
-
-
 (defun main (&optional (listen-address "tcp://*:4242"))
   (write-line "lisp server listening on port 4242")
   (pzmq:with-context nil
     (pzmq:with-socket responder :rep
       (pzmq:bind responder listen-address)
+      
       (loop
-        (let ((joske(cl-json:decode-json(pzmq:recv-string responder))))
-          (write-line (json-get joske '("1"))
-          ))
-          
+                  (let ((joske(yason:parse(pzmq:recv-string responder))))
+                    (loop for hashnumber in joske
+                          do(
+                            (write-line (gethash "author" hashnumber))
+                             (write-line  (gethash "category" hashnumber))  
+                              (write-line (gethash "author" hashnumber))
+                              (write-line (gethash "isEbook" hashnumber)) 
+                              (write-line (gethash "publisher" hashnumber)) 
+                           )
+                          )
+        )
         (pzmq:send responder (json:encode-json-to-string(make-instance 'book :title "The Witcher - Het Seizoen van Stormen" :author "Andrzej Sapkowski" :publisher "Luitingh Sijthoff Fantasy" :category "Fiction" :saleability "FOR_SALE" :isebook T :isbn "9781473232488" :coverurl "http://books.google.com/books/content?id=H2RIDgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")))))))
                                         ;(defun getbook()
                                         ;
